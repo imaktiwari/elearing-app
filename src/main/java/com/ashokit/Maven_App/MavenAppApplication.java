bdfe83f22e1782de0fd336bbfca30bdd47d9307d
Abhishek Kumar Tiwari <abhii.noida@gmail.com>
2025-02-06 12:21:48 +0530

    My First Commit

---
+++ src/main/java/com/ashokit/Maven_App/MavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit.Maven_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAppApplication.class, args);
+	}
+
+}
